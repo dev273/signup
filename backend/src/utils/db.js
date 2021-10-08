9289022e78d960a6f3917965755a2970030c6c9e
Parth Patel <201501015@daiict.ac.in>
2021-10-08 10:15:44 +0530

    Backend - Seed admin to DB

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -1,5 +1,23 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
+import User from '../models/user.model';
+
+const seedAdmin = async () => {
+  const adminName = process.env.DEFAULT_ADMIN_NAME;
+  const adminEmail = process.env.DETAULT_ADMIN_EMAIL;
+  const adminPassword = process.env.DEFAULT_ADMIN_PASSWORD;
+  const admin = await User.findOne({ email: adminEmail });
+  if (!admin) {
+    const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS);
+    const passwordHash = await bcrypt.hash(adminPassword, saltRounds);
+    await User.create({
+      name: adminName,
+      email: adminEmail,
+      password: passwordHash,
+      role: 'admin',
+    });
+  }
+};
 
 const connectDb = async () => {
   await mongoose.connect(process.env.MONGODB_URI, {
@@ -10,6 +28,7 @@ const connectDb = async () => {
 
 const applyDatabaseSetup = async () => {
   await connectDb();
+  await seedAdmin();
 };
 
 export default applyDatabaseSetup;
