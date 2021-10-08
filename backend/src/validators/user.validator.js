14577383435008ac0b429051f388c26fadac5a4f
Parth Patel <201501015@daiict.ac.in>
2021-10-08 10:55:41 +0530

    Backend - Add user validator

---
+++ backend/src/validators/user.validator.js
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+
+export const userCreateValidator = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  role: Joi.string().valid('user', 'admin').required(),
+});
+
+export const userUpdateValidator = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  role: Joi.string().valid('user', 'admin').required(),
+});
