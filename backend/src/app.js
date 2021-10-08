21fd4ae719c4c9b4dc4d31cb94636e926546e9f4
Parth Patel <201501015@daiict.ac.in>
2021-10-08 10:02:08 +0530

    Backend - Connected to Database

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import logger from 'morgan';
+import applyDatabaseSetup from './utils/db';
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// applyDatabaseSetup();
+applyDatabaseSetup();
 
 // app.use('/api/v1', routes);
 
